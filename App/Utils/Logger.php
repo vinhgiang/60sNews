633108fd13e4b0d5978ea62e8329147eb3d9b017
Vinh Giang <giangcamvinh@gmail.com>
2023-08-10 23:26:55 -0600

    use psr-4

---
+++ App/Utils/Logger.php
@@ -0,0 +1,23 @@
+<?php
+namespace src\Utils;
+
+class Logger
+{
+    /** @var string */
+    private $filePath = '';
+
+    public function __construct($filePath = 'log/info.log')
+    {
+        $this->filePath = $filePath;
+    }
+
+    public function log($msg)
+    {
+        $msg = '[' . Date('Y-m-d H:i:s') . "] $msg \n";
+
+        $existingLog = file_get_contents($this->filePath);
+        $existingLog = $existingLog !== false ? $existingLog : '';
+
+        file_put_contents($this->filePath, $existingLog . $msg);
+    }
+}
